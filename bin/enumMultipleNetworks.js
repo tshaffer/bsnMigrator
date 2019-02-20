e4888b358582c2d4bb84ffd2dcebb05e3232a8cf
Ted Shaffer <ted@brightsign.biz>
2019-02-20 12:16:44 -0800

    script to generate bpfs for multiple networks.

---
+++ bin/enumMultipleNetworks.js
@@ -0,0 +1,120 @@
+const util = require('util');
+const {inspect} = util;
+const fs = require('fs');
+
+const DM_DEV = true;
+let bsn = require('@brightsign/bsnconnector')
+
+const {bsnGetSession, BsnSessionStatus} = bsn;
+
+// If desired, create a json file for alternate credentials: ./test_data/credentials.json
+// Credentials object may also have an optional 'serverUrl' property to specify an alternate BSN url
+// let credentials;
+// try {
+//   credentials = require('../config/credentials.json');
+// } catch (error) {
+//   credentials = {
+//     user: 'chill@triggerpointmedia.com',
+//     password: 'admin',
+//     network: 'Spectrio-Rush',
+//     serverUrl: null,      // 'https://beta.brightsignnetwork.com:8443' // production test
+//   };
+// }
+
+// user: 'fvillegas@motionad.tv',
+// password: 'admin',
+// network: 'fvillegas',
+
+// user: 'rgardner@brightsign.biz',
+// password: 'admin',
+// network: 'Xfinity_Prod',
+
+// user: 'content@divmedia.net',
+// password: 'admin',
+// network: 'Diversified Media Group',
+
+// user: 'ted@brightsign.biz',
+// password: 'admin',
+// network: 'ted',
+
+// bsnGetSession returns singleton session - initializes if necessary
+// Call activate function to log in
+
+const credentialsSet = [
+  {
+    user: 'rgardner@brightsign.biz',
+    password: 'admin',
+    network: 'FHLBNY',
+    serverUrl: null,
+  },
+  {
+    user: 'rgardner@brightsign.biz',
+    password: 'admin',
+    network: 'DelphiDemo',
+    serverUrl: null,
+  }
+]
+
+const numNetworks = credentialsSet.length;
+var networkIndex = 0;
+
+var retrieveNetworkPresentations = (networkIndex) => {
+
+  const credentials = credentialsSet[networkIndex];
+
+  console.log('Logging In ...');
+  console.log(credentials);
+  bsnGetSession().activate(credentials.user, credentials.password, credentials.network, credentials.serverUrl)
+    .then((session) => {
+      // Not necessary, but here we look at session status and properties
+      console.log('Session Status: ', BsnSessionStatus[session.status]);
+      console.log(inspect(session, {depth: null, colors: true}));
+      console.log('');
+  
+      // Retrieve unfiltered presentation list
+      // This shows all presentation, including 'Simple' (old BSN WebUI) presentations
+      //  and the 'Disabled Players' presentation. Note that these do not have project files.
+      console.log('Getting all presentations ...');
+      return bsnGetSession().getPresentationList();
+    })
+    .then((items) => {
+  
+      console.log('All Presentations:');
+      console.log(inspect(items, {depth: null, colors: true}));
+      // console.log('');
+  
+      // Now we will get the list again, but with a filter string that will only give
+      //  us 'Complete' presentations with project files
+      let filterExpression = '[type] IS \'Complete\' AND [ProjectFile] IS NOT NULL';
+      console.log('Getting bpf/bpfx presentations ...');
+      return bsnGetSession().getPresentationList({filterExpression});
+    })
+    .then((items) => {
+      console.log('Filtered Presentations:');
+      // console.log(inspect(items, {depth: null, colors: true}));
+      // console.log('');
+  
+      var outputStr = '';
+      items.forEach( (item) => {
+        var itemStr = JSON.stringify(item);
+        outputStr = outputStr.concat(itemStr);
+        outputStr += '\n';
+      })
+      fs.writeFileSync('presentationFilesSpec.txt', outputStr);
+
+      bsnGetSession().deactivate();
+
+      console.log('network presentations complete');
+
+      networkIndex++;
+      if (networkIndex < numNetworks) {
+        console.log('move on to next network');
+        retrieveNetworkPresentations(networkIndex);
+      }
+      console.log('all networks complete');
+    })
+    .catch((error) => console.log('Error: ', error.message));
+  };
+
+  retrieveNetworkPresentations(0);
+
